6adb9bde5ae919d6e4fcd4174478326ad2b0e101
DianaHak <98120878+DianaHak@users.noreply.github.com>
2022-05-29 23:29:13 +0400

    Add files via upload
    Business landing page in Russian

---
+++ БизнесАс/js/script.js
@@ -0,0 +1,15 @@
+function review1click(){
+    review1.style.display = "block"
+    review2.style.display = "none"
+    review3.style.display = "none"
+}
+function review2click(){
+ review1.style.display = "none"
+    review2.style.display = "block"
+    review3.style.display = "none"
+}
+function review3click(){
+   review1.style.display = "none"
+    review2.style.display = "none"
+    review3.style.display = "block"
+}
